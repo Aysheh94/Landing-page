7fb46d427d8fbbed40d513df4d9efc84e5fecc93
Aysheh Alshishani <ayshehalshishani@Ayshehs-Laptop.attlocal.net>
2023-05-12 16:38:20 -0400

    Added A Scroll To Top Button

--- js/app.js
+++ js/app.js
@@ -23,6 +23,7 @@ const navList = document.querySelector('.navbar__list');
 const sections = document.querySelectorAll('section');
 // get offset value from root element 
 const rootElement = document.documentElement;
+const scrollToTopBtn = document.querySelector(".scrollToTopBtn");
 /**
  * End Global Variables
  * Start Helper Functions
@@ -67,7 +68,7 @@ hamburger.addEventListener('click',() => {
 
 
 // Add class 'active' to section when near top of viewport
-const addActiveClass= () => {
+const addActiveClass = () => {
     sections.forEach((section) => {
         const position= section.getBoundingClientRect();
         const sectiosName=section.getAttribute("data-nav");
@@ -91,7 +92,27 @@ const addActiveClass= () => {
 
 window.addEventListener("scroll",addActiveClass);
 
+//scroll to top function
+const handleScroll = () => {
+    const scrollTotal=rootElement.scrollHeight- rootElement.clientHeight;
+    if(rootElement.scrollTop/scrollTotal > 0.8){
+        scrollToTopBtn.classList.add("showBtn");
+    }else{
+        scrollToTopBtn.classList.remove("showBtn");
+    }
+}
 
+const scrollToTop = () => {
+    rootElement.scrollTo({
+        top:0,
+        behavior:"smooth"
+    });
+};
+
+
+//scroll to top event
+scrollToTopBtn.addEventListener('click',scrollToTop);
+document.addEventListener("scroll",handleScroll);
 
 /**
  * End Main Functions
